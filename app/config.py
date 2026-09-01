f894ba438866da6e14e940a0051679d12737652d
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-09-01 13:08:14 +0200

    changed url

--- app/config.py
+++ app/config.py
@@ -2,10 +2,11 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('GISYNC_DATABASE_URL', 'sqlite:///./database.db')
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///./database.db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
