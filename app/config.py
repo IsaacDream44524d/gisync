390d35cd8531ede716f57d8e336868411997af4b
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-09-01 14:32:50 +0200

    fixed url

--- app/config.py
+++ app/config.py
@@ -4,9 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "sqlite:///./database.db"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
